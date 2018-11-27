7e21abdc86b341ef31fc1ef95c899dcd14465788
nemo <slbaker@hotmail.co.nz>
2018-11-28 10:37:34 +1300

    Update ccminer-config-win.h
    define

--- ccminer-config-win.h
+++ ccminer-config-win.h
@@ -156,7 +156,7 @@
 #define PACKAGE_NAME "ccminer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ccminer 8.21-KlausT"
+#define PACKAGE_STRING "ccminer KlausT/yescrypt/cuda10"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ccminer"
@@ -165,7 +165,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "8.21-KlausT"
+#define PACKAGE_VERSION "KlausT/yescrypt/cuda10"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
